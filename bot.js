84107478112d7a4a6d15a463800cf8b709b0e5eb
xert <admin@xert.xyz>
2015-08-31 17:47:49 +0000

    bot made

--- bot.js
+++ bot.js
@@ -23,4 +23,55 @@ if (typeof String.prototype.startsWith != 'function') {
 }
 
 
+// Let's start defining the IRC Bot
 
+
+// Here's the config...
+
+var config = {
+    channels: ["#digitaladdiction"],
+    server: "irc.digitaladdiction.info",
+    userName: "WeatherGuy",
+    realName: "WeatherGuy",
+};
+
+// Here's the bot...
+
+var bot = new irc.Client(config.server, config.userName, {
+    channels: config.channels,
+    realName: config.realName,
+    userName: config.userName,
+    floodProtection: true
+    });
+    
+    
+// TODO: Add realtime functionality
+
+
+
+// On a message, do something or another...
+
+bot.addListener("message", function(from, to, text, message){
+    
+    // Sanitize the contents of each value
+    
+    //make a local variable out of the messgae's text
+    var theText = text.toString();
+    
+    //make the message lowercase, so it can be ANyThiNG the stupid user puts in.
+    
+    theText = theText.toLowerCase();
+    
+    
+    // same comment as above
+    var theCommand = text.substr(0, theText.indexOf(' '));
+    theCommand = theCommand.toLowerCase();
+    
+    
+    var strFrom = from.toString();
+    
+    
+
+
+
+});
