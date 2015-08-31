2063958a5c0b96a72bcf4c6c4d67907db5ce4e08
xert <admin@xert.xyz>
2015-08-31 17:30:55 +0000

    3

--- basics.js
+++ basics.js
@@ -5,3 +5,19 @@ basics.js
 includes basic functionality for some stuff. 
 
 */
+
+
+// Add Round functionality
+
+function round(value)
+{
+    value = +value;
+    if (isNan(value))
+    {
+        return NaN;
+    }
+    value = value.toString().split('e');
+    value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + 2): 2)));
+    value = value.toString().split('e');
+   
+}
--- bot.js
+++ bot.js
@@ -8,3 +8,18 @@ Xert of DA
 
 */
 
+var basics = require("/basics.js");
+
+
+
+// Add StartsWith functionality
+
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) === 0;
+  };
+}
+
+
+
